623049e8bd4e1dbdae68bafabae7220f72906e54
apyd <arturpydych@gmail.com>
2024-03-20 15:45:14 +0100

    feat: add users related utils

---
+++ src/api/users/users.utils.ts
@@ -0,0 +1,31 @@
+import { CreateUserDto } from "./users.dto.js"
+
+export const extendUserWithLinks = (user) => {
+  if(!user) return null
+  return {
+    user: {
+      ...user
+    },
+    links: {
+      self: `/api/users/${user.id}`,
+      hobbies: `/api/users/${user.id}/hobbies`,
+    },
+  }
+}
+
+export const validateCreateUserDto = (user: CreateUserDto): boolean | void => {
+  if (!user.name || user.name.trim() === '' || typeof user.name !== 'string') {
+    throw new Error('Name is required and must be a string')
+  }
+  if (!user.email || !user.email.includes('@') || typeof user.email !== 'string') {
+    throw new Error('Email is required and must be a real email address')
+  }
+  return true
+}
+
+export const validateUpdateUserHobbiesDto = (hobbies: string[]): boolean | void => {
+  if (!hobbies || !Array.isArray(hobbies)) {
+    throw new Error('Hobbies are required and must be an array of strings')
+  }
+  return true
+}
