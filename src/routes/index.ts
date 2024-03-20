db6c80b7f1d11fc7962d56777ea3bcb1a81d925a
apyd <arturpydych@gmail.com>
2024-03-20 15:45:53 +0100

    feat: add db, routes and constants

---
+++ src/routes/index.ts
@@ -0,0 +1,21 @@
+import http from 'http'
+
+import { usersController } from "../api/users/users.controller.js"
+import { notFoundController } from '../api/common/notFound.controller.js'
+import { validatePath } from "../utils/index.js"
+
+const routingMapper = {
+  users: usersController
+}
+
+export const router = async (req: http.IncomingMessage, res: http.ServerResponse) => {
+  const path = new URL(req.url!, `http://${req.headers.host}`).pathname
+  const pathElements = path?.split('/').filter(pathElement => pathElement) || []
+  const isPathValid = validatePath(pathElements)
+  if (!isPathValid) {
+    notFoundController(res)
+    return
+  }
+  const controller = routingMapper?.[pathElements[1]]
+  await controller(req, res)
+}
