401b11639e6a79b4a9fc797dd7d0df931df33c5d
apyd <arturpydych@gmail.com>
2024-03-20 15:46:18 +0100

    feat: add server implementation

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import http from 'node:http';
+import { router } from './routes/index.js';
+
+const app = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => await router(req, res))
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${process.env.PORT}`)
+})
